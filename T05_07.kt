d9782fc3c78c22ffd68430b8eb5292f1cbd3d7a3
설경인 <66372110+Seol-kyoungin@users.noreply.github.com>
2024-05-07 16:59:20 +0900

    Update K05_07.kt
    1부터 100까지 짝수의 합만 출력

--- T05_07.kt
+++ T05_07.kt
@@ -1,3 +1,4 @@
+//for
 fun main() {
     var sum = 0
     for (i in 1..101) {
@@ -6,4 +7,21 @@ fun main() {
         }
     }
     println(sum)
+}
+
+
+//while
+fun main() {
+    var a = 1
+    var sum = 0
+    
+    while (a <= 100) {
+        if (a % 2 == 0) {
+            sum += a
+             
+        }
+       
+       a++
+    }
+     println(sum)
 }
