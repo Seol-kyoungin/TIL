791aa2fd545ef775a9ff5392e65bc52d45a2abd8
설경인 <66372110+Seol-kyoungin@users.noreply.github.com>
2024-05-07 17:14:06 +0900

    Update T05_07.kt

--- T05_07.kt
+++ T05_07.kt
@@ -25,3 +25,20 @@ fun main() {
     }
      println(sum)
 }
+
+
+
+//홀수의 *1-n까지 출력
+
+fun main() {
+    val n = 3 // n까지 출력
+
+    for (a in 1..n) {
+        if (a % 2 != 0) { // 홀수인 경우
+            for (count in 1..a) {
+                print("*")
+            }
+            println()
+        }
+    }
+}
